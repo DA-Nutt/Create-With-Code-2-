61df4210f1aa7d71e00b35da568a14d7af5bba49
DevonNutting <devon.nutting@hgs.hiddengeniusproject.org>
2023-05-05 16:05:48 -0700

    completed 2.3

--- AnimalMircoGame/Assets/Scripts/PlayerController.cs
+++ AnimalMircoGame/Assets/Scripts/PlayerController.cs
@@ -8,15 +8,18 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10.0f;
     public float xRange = 15.0f; // the Max distance the player move within in the scene
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject projectileObject; 
 
     // Update is called once per frame
     void Update()
     {
+        xInput = Input.GetAxis("Horizontal"); // Left & Right keys + A & D Keys
+        transform.Translate(Vector3.right * xInput * Time.deltaTime * moveSpeed);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Instantiate(projectileObject, transform.position, projectileObject.transform.rotation);
+        }
+
         if (transform.position.x < -xRange) { // If the player is out of bounds on the left side
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
@@ -24,9 +27,6 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange) { // If the player is out of bounds on the right side
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-
-        xInput = Input.GetAxis("Horizontal"); // Left & Right keys + A & D Keys
-
-        transform.Translate(Vector3.right * xInput * Time.deltaTime * moveSpeed); 
+ 
     }
 }
---
+++ AnimalMircoGame/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    public float spawnRangeX = 20.0f;
+    public float spawnPosZ = 20.0f;
+
+    private void Start()
+    {
+       StartCoroutine(SpawnRandomAnimal());
+       StartCoroutine(SayHello());
+    }      
+
+    IEnumerator SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0 , spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        yield return new WaitForSeconds(1);
+    }
+
+    IEnumerator SayHello(){
+        Debug.Log("Hello World!");
+        yield return new WaitForSeconds(1f);
+    }
+
+}
