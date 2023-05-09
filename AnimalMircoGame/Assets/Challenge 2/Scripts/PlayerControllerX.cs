2eb2411f8846ad5b72a17be0b029554619c43d1e
DevonNutting <devon.nutting@hgs.hiddengeniusproject.org>
2023-05-08 18:55:39 -0700

    Completed Challenge 2

---
+++ AnimalMircoGame/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float spawnInterval = 1f;
+    private float lastSpawnTime; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnDog();
+        }
+    }
+
+    public bool CanSpawn(){
+        if(Time.time - lastSpawnTime < spawnInterval){
+            return false;
+        }
+
+        return true;
+    }
+
+    public void SpawnDog(){
+        if(CanSpawn()){
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawnTime = Time.time;
+        }
+        
+    }
+}
